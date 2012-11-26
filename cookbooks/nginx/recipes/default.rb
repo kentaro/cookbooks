014c86383d4ba2ff0c8ad22fbab3c495a6f93822
Kentaro Kuribayashi <kentarok@gmail.com>
2012-11-26 23:36:40 +0900

    fix nginx

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -18,11 +18,6 @@ node['nginx']['install_pkgs'].each do |p|
 end
 
 service 'nginx' do
-  case node[:platform]
-  when 'ubuntu'
-    if node[:platform_version].to_f >= 9.10
-      provider Chef::Provider::Service::Upstart
-    end
-  end
+  supports :status => true, :restart => true, :reload => true
   action [:enable, :start]
 end
